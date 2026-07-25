e1f716c330802d977d572e4e09b8eb1c1bc5a73d
MaxAmerico <maxdarosa561@gmail.com>
2026-07-25 17:39:00 -0300

    i can't understand how my code worked

--- main/Primerbook challenges/Challenge 3(sum numbers from 50 to 100).cpp
+++ main/Primerbook challenges/Challenge 3(sum numbers from 50 to 100).cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 // I am still trying to learn this
+//wtf i can't understand how my code actually worked
 int main() {
-    int sum = 1;
+    int sum = 50;
     int value = 50;
 
-    while (sum <= 50) {
-        value = sum + value;
+    while (sum <= 100) {
+        value+=sum;
         ++sum;
         std::cout << "The result is" << " " << value << " ";
     }
