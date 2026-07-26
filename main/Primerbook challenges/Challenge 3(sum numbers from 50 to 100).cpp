3dd44e05fd14e411701a96360300bc1dad2cba94
MaxAmerico <maxdarosa561@gmail.com>
2026-07-25 22:07:24 -0300

    New exercice done about while

--- main/Primerbook challenges/Challenge 3(sum numbers from 50 to 100).cpp
+++ main/Primerbook challenges/Challenge 3(sum numbers from 50 to 100).cpp
@@ -1,19 +1,20 @@
+//Exercise 1.11: Write a program that prompts the user for two integers.
+//Print each number in the range specified by those two integers.
+
 #include <iostream>
 
-// I am still trying to learn this
-//wtf i can't understand how my code actually worked
 int main() {
-    int sum = 50;
-    int value = 50;
+    int number1, number2;
 
-    while (sum <= 100) {
-        value = value + sum;
-        ++sum;
-        std::cout << "The result is" << " " << value << " ";
-    }
-    return 0;
-}
-//WOW now i can get it, it happens that sum the value plus the sum and then, it gets sum + 1,
-//I gonna try to explain, the sum = 50, and the statement while needs to go until the sum = 100,
-//the first round starts off 50(value) = 50(value)+ 50(sum), getting 50 = 100, afterwards on second round, 100 = 100(value) + 51(sum)
-//i.e 100 = 151; on third round, 151 = 151(value) + 52(sum) = 203, until sum = 100, finishing with 3875
+     std::cout << "Number 1 and Number 2:";
+     std::cin >> number1 >> number2;
+      
+     std::cout << "The range between " << number1 << " " << "and" << " ";
+     std::cout << number2 << " " << "is" << " ";
+      while(number2 - 1 > number1) {
+        number1 = number1 + 1;
+        std::cout <<  number1 << " ";
+      }
+        
+      return 0;
+} 
---
+++ main/Primerbook challenges/Challenge 5(While exercicice most info within the code).cpp
@@ -0,0 +1,2 @@
+//Exercise 1.11: Write a program that prompts the user for two integers.
+Print each number in the range specified by those two integers.
