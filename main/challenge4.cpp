c11424c163d75eaafb4bb6635857f642d95f9192
MaxAmerico <maxdarosa561@gmail.com>
2026-07-27 22:53:37 -0300

    NOW -> I have to find out the factorial of a number(hell)

---
+++ main/challenge4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    std::cout << "Choose a number:" << " ";
+    std::cin >> number;
+     std::cout << "The factorial of" << " " << number << " " << "is:" << " ";
+     while(number <  ) {
+      ;
+     }
+     std::cout << number;
+      
+}
