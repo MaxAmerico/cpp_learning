256a936231d30bca0964e4ec7a22a50b8253244b
MaxAmerico <maxdarosa561@gmail.com>
2026-08-01 22:51:13 -0300

    I can't solve a challenge

---
+++ main/AnotherChallenges/Challenge4.cpp
@@ -0,0 +1,5 @@
+//Challenge 4 — Divisors
+
+//Read a number.
+//Print all its divisors.
+
---
+++ main/AnotherChallenges/Challenge5.cpp
@@ -0,0 +1,14 @@
+//Read a number N.
+//Find the smallest number greater than N that is divisible by both 7 and 5.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while ()
+    return 0;
+}
+//I can't do this too.
