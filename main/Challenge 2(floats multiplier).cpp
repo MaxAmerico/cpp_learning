9e26e14cc86adaed15cd0658a059f89253a6140f
MaxAmerico <maxdarosa561@gmail.com>
2026-08-03 22:41:03 -0300

    Omg I commited so many mistakes

--- main/Challenge 2(floats multiplier).cpp
+++ main/Challenge 2(floats multiplier).cpp
@@ -4,10 +4,10 @@ int main() {
     float number2;
 
     std::cout << "Number1:";
-    std::cin >> numero1;
+    std::cin >> number1;
     
     std::cout << "Number2:";
-    std::cin >> numero2;
+    std::cin >> number2;
 
     std::cout << "Result:" << number1 * number2;
 
